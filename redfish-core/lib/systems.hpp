b1120a56e7f02b46e4fc9c8b8281b179830e3257
Chris Cain <cjcain@us.ibm.com>
2024-05-16 14:27:32 -0500

    Systems: Add OEM safe mode to user interface
    Tested: Forced safe mode and verified dbus was updated
    
    Change-Id: Ia2e23154454025ca7a8107e54f2de3883363490a
    Signed-off-by: Chris Cain <cjcain@us.ibm.com>

--- redfish-core/lib/systems.hpp
+++ redfish-core/lib/systems.hpp
@@ -1583,9 +1583,10 @@ inline void
 
     std::string powerMode;
     const std::vector<std::string>* allowedModes = nullptr;
+    std::optional<bool> safeMode;
     const bool success = sdbusplus::unpackPropertiesNoThrow(
         dbus_utils::UnpackErrorPrinter(), properties, "PowerMode", powerMode,
-        "AllowedPowerModes", allowedModes);
+        "AllowedPowerModes", allowedModes, "SafeMode", safeMode);
 
     if (!success)
     {
@@ -1625,6 +1626,15 @@ inline void
         return;
     }
     asyncResp->res.jsonValue["PowerMode"] = modeValue;
+
+    if (safeMode)
+    {
+        BMCWEB_LOG_DEBUG("Safe mode: {}", *safeMode);
+        nlohmann::json& oemSafeMode = asyncResp->res.jsonValue["Oem"];
+        oemSafeMode["@odata.type"] = "#OemComputerSystem.Oem";
+        oemSafeMode["IBM"]["@odata.type"] = "#OemComputerSystem.IBM";
+        oemSafeMode["IBM"]["SafeMode"] = *safeMode;
+    }
 }
 
 /**
